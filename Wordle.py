acfffac82d1a48c1ccaed18016b659222715b5a1
meh0013 <mehulsaxena16@gmail.com>
2025-02-22 11:57:43 +0530

    little tweaks-might be too hard now though...

--- Wordle.py
+++ Wordle.py
@@ -1,87 +1,14 @@
 #Wordle
-
-'''
-Strings are guessed and updated to a 5x5 matrix(guesser function) 
- -> filter the wordlist to 5 letter words only 
-Positional verification function(green - right word right place, yellow - right word wrong place, red - wrong word) 
- -> how to give colour to text on command line? -> can use termcolor but that doesn't allow comparision
- -> alternative: use ANSI sequences, which allow comparision
-	-> Instead of applying it to each word individually, create a class that does just that
-	-> each string+it's ANSI color code becomes an object, allowing comparision between them
-	-> but compare method must be specified else it'll not work -> operator overloading
-
-BACKGROUND_RED = '\033[41m'
-BACKGROUND_GREEN = '\033[42m'
-BACKGROUND_YELLOW = '\033[43m' # orange on some systems
-'''
-'''class ColoredText:
-    def __init__(self, text, color):
-        self.text = text
-        self.color = color  # Store the color name or code
-
-    def __repr__(self):
-        color_code = {
-            "red": "\033[31m",
-            "green": "\033[32m",
-            "blue": "\033[34m",
-            "reset": "\033[0m"
-        }
-        return f"{color_code.get(self.color, '')}{self.text}{color_code['reset']}" #(coloring start)(text)(end coloring)
-    
-    def __iter__(self):
-        # Yield the attributes in sequence
-        yield self.text
-        yield self.color
-
-    def compare(self, other):
-        return self.text == other.text and self.color == other.color
-		#only if both are true, returns true
-		#don't really need to check the text, but it's better to have there as a failsafe'''
-
-
-'''import os
-import random
-os.system('color')
-from termcolor import colored
-
-
-	
-def word_compare(guess, answer):
-	result=""
-	for i in range(5): 
-		if guess[i]==answer[i]: 
-			result+=colored(guess[i],"green")
-		elif guess[i] in answer:
-			result+=colored(guess[i],"yellow")
-		else:
-			result+=colored(guess[i],"red")	
-
-	return result
-
-
-def wordle():
-	global flag,guess,answer,guess_list
-	flag=0
-	answer=random.choice(open("fiveletterwords.txt","r").read().split())
-	print(answer)
-	while flag==0:
-		guess_list=[]		
-		guess=str(input("_ _ _ _ _\n"))
-		guess_list.append(word_compare(guess.lower(), answer))
-		print(*guess_list, sep="\n")
-		if guess==answer: flag=1
-
-wordle()'''
-
-
-
-
 import os
 import random
 os.system('color')
+import nltk
+from nltk.corpus import words,wordnet
 from termcolor import colored
 
-
+#Download required datasets
+nltk.download("words")
+nltk.download("wordnet")
 	
 def word_compare(guess, answer):
 	result=""
@@ -95,31 +22,50 @@ def word_compare(guess, answer):
 
 	return result
 
-
 def wordle():
-	global flag,guess,answer,guess_list
-	flag=0
-	guess_list=[]
-	
-	answer=random.choice(open("fiveletterwords.txt","r").read().split())
-	print(answer)
-	
-	while flag==0:
-		guess = input("_ _ _ _ _\n").lower()
-		print("_ _ _ _ _")
-		if len(guess) == 5:  
-            		result = word_compare(guess, answer)
-            		guess_list.append(result)  
+    global flag,guess,answer,guess_list
+    flag=0
+    guess_list=[]
+    word_list=[word.lower() for word in words.words() if len(word)==5]
+    answer=random.choice(word_list)
+    
+    def get_word_meaning(word):
+        synsets=wordnet.synsets(word)
+        if synsets:
+            return synsets[0].definition()
+        return "Meaning not found"
+    
+    #answer=random.choice(open("fiveletterwords_final.txt","r").read().split())
+    while flag==0:
+        guess=input("_ _ _ _ _\n").lower()
+        print("_ _ _ _ _")
+        if len(guess)==5:
+            
+            if len(guess_list)==5:
+                print("You lost! The word was: ",answer)
+                print("Meaning of your word: ",get_word_meaning(answer))
+                break
+            
+            if guess not in open("fiveletterwords_final.txt","r").read().split():
+                print("Given word does not exist...please try again")
+                continue
+            
+            result=word_compare(guess,answer)
+            guess_list.append(result)
+            
+            if guess==answer:
+                print("Congratulations! You've guessed the word!")
+                flag=1
+            
+            for previous_guess in guess_list:
+                print(previous_guess)
 
-            		
-            		for previous_guess in guess_list:
-                		print(previous_guess)
-
-            		if guess == answer:
-                		print("Congratulations! You've guessed the word.")
-                		flag = 1
-		else:
-            		print("Please enter a valid 5-letter word.")
-
+                   
+        else:
+            print("Please enter a valid 5-letter word.")
+            
 wordle()
+
+
+
 
